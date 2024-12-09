7a2522c3aa255f09347c535ef9d3614d48a8e206
poorna30646 <thotipoornachandra@gmail.com>
2024-12-09 10:55:21 +0530

    Add files via upload

---
+++ thriceindices.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class fire
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int ns[]=new int[n];
+        int k=0;int s=0;
+        for(int i=1;i<n;i++)
+        {
+            s+=i;
+            ns[k]=s;
+            k++;
+        }
+        for(int i=0;i<n;i+=3)
+        {
+            System.out.println(ns[i]+" ");
+        }
+        
+    }
+}
+   
